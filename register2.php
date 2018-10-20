659bbd8562418a691d27f662a99bd33a54bc33b4
ALLEN K ABRAHAM <allenabraham777@gmail.com>
2018-10-20 12:30:03 +0530

    SOME MODIFICATIONS

--- register2.php
+++ register2.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect('localhost','root','','');
+$conn = mysqli_connect('localhost','root','','thedavincicode');
 if (!$conn) {
     die("Connection failed: " . $conn->connect_error);
     //echo "conn failed";
@@ -37,19 +37,20 @@ $phone_no = $_POST['phone_no'];
 } 
     if(isset($_POST) & !empty($_POST)){
 	$team_name = mysqli_real_escape_string($conn, $_POST['team_name']);
- 	$sql = "SELECT * FROM `resgistration` WHERE team_name='$team_name'";
+ 	$sql = "SELECT * FROM `registration` WHERE team_name='$team_name'";
 	$result = mysqli_query($conn, $sql);
 	$count = mysqli_num_rows($result);
-	if($count == 1){
+	if($count >= 1){
 		echo "Team Name Not Available";
 	}
 	else{
+		if($team_name!="")
 		echo "Team Name Available";
 	        }
 
 	}		
 	        if (isset($_POST['submit'])){
-             $sql = "INSERT INTO resgistration (team_name,leader_name,email,phone_no) VALUES ('$team_name','$leader_name','$email',$phone_no)";
+             $sql = "INSERT INTO registration (team_name,leader_name,email,phone_no) VALUES ('$team_name','$leader_name','$email','$phone_no')";
 			if (!mysqli_query($conn,$sql)) {
 			    echo "Not registered!";
 			} else {
